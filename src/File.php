499fc4560d5525f473087f53742680b17836df93
Yang Ming <yangming0116@163.com>
2016-04-04 19:39:12 +0800

    增加隐藏文件开关

--- src/File.php
+++ src/File.php
@@ -256,7 +256,7 @@ class File {
      * @param string $fsencoding 文件系统的编码，如不为空则会自动进行一些编码转换
      * @return array 目录中的文件列表
      */
-    public static function ls($full, $fsencoding = '') {
+    public static function ls($full, $fsencoding = '', $hidden = false) {
         $full = self::conv_to($full, $fsencoding);
         if (substr($full, -1) !== '/') {
             $full .= '/';
@@ -266,6 +266,9 @@ class File {
             if ($dh = opendir($full)) {
                 while (($file = readdir($dh)) !== false) {
                     if ($file === '.' || $file === '..') {
+                        continue;
+                    }
+                    if ($file{0} == '.' && !$hidden) {
                         continue;
                     }
                     $filename = self::conv_from($file, $fsencoding);
