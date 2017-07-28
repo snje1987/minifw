645785129418f4eb3609dd6587f289fdf37a9599
Yang Ming <yangming0116@163.com>
2017-07-28 16:43:24 +0800

    add some sample file

---
+++ config.sample.php
@@ -0,0 +1,23 @@
+<?php
+
+$cfg['mysql'] = [
+    'host' => 'localhost',
+    'username' => '',
+    'password' => '',
+    'dbname' => '',
+    'encoding' => 'utf8',
+];
+
+$cfg['debug']['debug'] = 0;
+$cfg['main']['dbprefix'] = '';
+$cfg['main']['theme'] = '';
+$cfg['main']['db'] = 'Mysqli';
+
+$cfg['path'] = [
+    'theme' => '/theme',
+    'theme_res' => '/www',
+    'compiled' => '/compiled',
+    'ttfs' => '/ttfs',
+    'web_root' => __DIR__,
+];
+
