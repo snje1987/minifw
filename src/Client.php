e65907f27c47464c3ce72f6f10e75a6641ee059b
Yang Ming <yangming0116@163.com>
2017-08-26 10:08:34 +0800

    移除源码中的版权注释

--- src/Client.php
+++ src/Client.php
@@ -1,37 +1,7 @@
 <?php
 
-/*
- * Copyright (C) 2013 Yang Ming <yangming0116@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
- * @filename Client.php
- * @encoding UTF-8
- * @author Yang Ming <yangming0116@gmail.com>
- * @copyright Copyright (C) 2013 杨明
- * @datetime 2013-3-26 15:43:11
- * @version 1.0
- * @Description 一些网络的操作，包括get和post
- */
-
 namespace Org\Snje\Minifw;
 
-/**
- * 网络操作客户端
- */
 class Client {
 
     public $timeout = 30;
--- tests/Table/DiffTest.php
+++ tests/Table/DiffTest.php
@@ -1,32 +1,10 @@
 <?php
 
-/*
- * Copyright (C) 2016 Yang Ming <yangming0116@163.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 namespace Org\Snje\MinifwTest\Table;
 
 use Org\Snje\Minifw as FW;
 use Org\Snje\MinifwTest as Ts;
 
-/**
- * Description of ManageTest
- *
- * @author Yang Ming <yangming0116@163.com>
- */
 class DiffTest extends Ts\TestCommon {
 
     public function test_add() {
