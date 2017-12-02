7c0c7c723d670bab9d3822485b5136e1200386b3
Yang Ming <yangming0116@163.com>
2017-12-02 12:59:36 +0800

    增强随机数生成算法

--- src/Random.php
+++ src/Random.php
@@ -14,10 +14,35 @@ class Random {
     protected static $alpha_len;
     protected static $digit_len;
 
+    public static function gen_int($min, $max) {
+        if (function_exists('random_int')) {
+            return random_int($min, $max);
+        }
+        return mt_rand($min, $max);
+    }
+
+    public static function gen_byte($len, $bin = false) {
+        $byte = null;
+        if (function_exists('random_bytes')) {
+            $byte = random_bytes($length);
+        }
+        if (function_exists('mcrypt_create_iv')) {
+            $byte = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
+        }
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $byte = openssl_random_pseudo_bytes($length);
+        }
+        if ($bin) {
+            return $byte;
+        } else {
+            return bin2hex($byte);
+        }
+    }
+
     public static function gen_key($len) {
         $key = '';
         for ($i = 0; $i < $len; $i++) {
-            $key .= self::$chars[mt_rand(0, self::$char_len - 1)];
+            $key .= self::$chars[self::gen_int(0, self::$char_len - 1)];
         }
         return $key;
     }
@@ -25,7 +50,7 @@ class Random {
     public static function gen_str($len) {
         $key = '';
         for ($i = 0; $i < $len; $i++) {
-            $key .= self::$alphas[mt_rand(0, self::$alpha_len - 1)];
+            $key .= self::$alphas[self::gen_int(0, self::$alpha_len - 1)];
         }
         return $key;
     }
@@ -33,7 +58,7 @@ class Random {
     public static function gen_num($len) {
         $key = '';
         for ($i = 0; $i < $len; $i++) {
-            $key .= self::$digits[mt_rand(0, self::$digit_len - 1)];
+            $key .= self::$digits[self::gen_int(0, self::$digit_len - 1)];
         }
         return $key;
     }
