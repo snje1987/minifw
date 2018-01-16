2db0566afd2f2c3a93a2d7566329d75d9aa5e497
Yang Ming <yangming0116@163.com>
2018-01-16 19:49:28 +0800

    正确处理表结构中的单引号

--- tests/Table/TableAdd.php
+++ tests/Table/TableAdd.php
@@ -20,14 +20,14 @@ class TableAdd extends FW\Table {
     public static $field = array(
         'id' => array('type' => 'int(10) unsigned', 'extra' => 'auto_increment', 'comment' => 'ID'),
         'intfield' => array('type' => 'int(11)', 'comment' => 'A int field'),
-        'charfield' => array('type' => 'varchar(200)', 'comment' => 'A varchar field'),
+        'charfield' => array('type' => 'varchar(200)', 'comment' => 'A \' varchar field'),
         'textfield' => array('type' => 'text', 'comment' => 'A text field'),
         'addfield' => array('type' => 'text', 'comment' => 'A add field'),
         'intfield_def' => array('type' => 'int(11)', 'default' => '0', 'comment' => 'A int field'),
-        'charfield_def' => array('type' => 'varchar(200)', 'default' => '', 'comment' => 'A varchar field'),
+        'charfield_def' => array('type' => 'varchar(200)', 'default' => '123\'', 'comment' => 'A varchar field'),
     );
     public static $index = array(
-        'PRIMARY' => array('fields' => array('id'), 'comment' => '主键'),
+        'PRIMARY' => array('fields' => array('id'), 'comment' => '主\'键'),
         'intfield' => array('fields' => array('intfield', 'charfield')),
         'uniqueindex' => array('unique' => true, 'fields' => array('intfield')),
         'addfield' => array('fields' => array('charfield')),
@@ -38,8 +38,8 @@ class TableAdd extends FW\Table {
             'trans' => 'ALTER TABLE `table_with_one` ADD `id` int(10) unsigned NOT NULL auto_increment COMMENT \'ID\' first;',
         ),
         array(
-            'diff' => '+[2] `charfield` varchar(200) NOT NULL COMMENT \'A varchar field\'',
-            'trans' => 'ALTER TABLE `table_with_one` ADD `charfield` varchar(200) NOT NULL COMMENT \'A varchar field\' after `intfield`;',
+            'diff' => '+[2] `charfield` varchar(200) NOT NULL COMMENT \'A \'\' varchar field\'',
+            'trans' => 'ALTER TABLE `table_with_one` ADD `charfield` varchar(200) NOT NULL COMMENT \'A \'\' varchar field\' after `intfield`;',
         ),
         array(
             'diff' => '+[3] `textfield` text NOT NULL COMMENT \'A text field\'',
@@ -54,12 +54,12 @@ class TableAdd extends FW\Table {
             'trans' => 'ALTER TABLE `table_with_one` ADD `intfield_def` int(11) NOT NULL DEFAULT \'0\' COMMENT \'A int field\' after `addfield`;',
         ),
         array(
-            'diff' => '+[6] `charfield_def` varchar(200) NOT NULL DEFAULT \'\' COMMENT \'A varchar field\'',
-            'trans' => 'ALTER TABLE `table_with_one` ADD `charfield_def` varchar(200) NOT NULL DEFAULT \'\' COMMENT \'A varchar field\' after `intfield_def`;',
+            'diff' => '+[6] `charfield_def` varchar(200) NOT NULL DEFAULT \'123\'\'\' COMMENT \'A varchar field\'',
+            'trans' => 'ALTER TABLE `table_with_one` ADD `charfield_def` varchar(200) NOT NULL DEFAULT \'123\'\'\' COMMENT \'A varchar field\' after `intfield_def`;',
         ),
         array(
-            'diff' => '+ PRIMARY KEY (`id`) COMMENT \'主键\'',
-            'trans' => 'ALTER TABLE `table_with_one` ADD PRIMARY KEY (`id`) COMMENT \'主键\';',
+            'diff' => '+ PRIMARY KEY (`id`) COMMENT \'主\'\'键\'',
+            'trans' => 'ALTER TABLE `table_with_one` ADD PRIMARY KEY (`id`) COMMENT \'主\'\'键\';',
         ),
         array(
             'diff' => '+ INDEX `intfield` (`intfield`,`charfield`)',
